1387055e4f73e8e11015130a495c99b930ec0ec2
mzdemir <mzdemir0@gmail.com>
2025-01-10 15:57:52 +0300

    -

---
+++ patika-react-odev/src/lib/service.js
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+const getData1 = async (user_id) => {
+  const { data: users } = await axios(
+    `https://jsonplaceholder.typicode.com/users/${user_id}`
+  )
+  const { data: posts } = await axios(
+    `https://jsonplaceholder.typicode.com/posts?id=${user_id}`
+  )
+
+  return { users, posts }
+}
+
+export default getData
+
